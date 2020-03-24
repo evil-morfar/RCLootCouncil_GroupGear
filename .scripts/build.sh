bf2f68e956d699ca9e928d48469414cbbd9ae731
evil_morfar <evil_morfar@hotmail.com>
2020-03-24 21:02:13 +0100

    Updated classic .toc to 11304

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -3,4 +3,4 @@ echo "Executing a $0"
 release_script="./.scripts/release.sh"
 
 "$release_script" -r "$(pwd)/.tmp/Retail" -doz
-"$release_script" -r "$(pwd)/.tmp/Classic" -doz -g 1.13.3
+"$release_script" -r "$(pwd)/.tmp/Classic" -doz -g 1.13.4
