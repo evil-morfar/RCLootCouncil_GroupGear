f24f026c7e8e8bc0368f72e8e9b4b638a4461a91
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2025-07-08 20:57:50 +0200

    v2.7.2 - build should now be fixed

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -5,12 +5,12 @@ release_script=$(curl -s "https://raw.githubusercontent.com/BigWigsMods/packager
 
 # # -d: Skip Upload
 # # -z: Skip zip
-echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Retail" -do
-echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Classic" -do -g 1.13.4
+echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Retail" -doz
+# echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Classic" -do -g 1.13.4
 
 # # Move the zip(s)
-mv ./.tmp/Retail/*.zip "./.release/"
-mv ./.tmp/Classic/*.zip "./.release/"
+# mv ./.tmp/Retail/*.zip "./.release/"
+# mv ./.tmp/Classic/*.zip "./.release/"
 #
 # # And delete .tmp
 # rm -r "./.tmp"
